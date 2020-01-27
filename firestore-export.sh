bbc67eddf6ba3200c0be9f13a2dab5a964ae81e3
Matt Davies <mattdavies618@gmail.com>
2020-01-27 09:26:13 +0000

    Added node script to firestore export shell script

--- firestore-export.sh
+++ firestore-export.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 gcloud config set project $GCLOUD_PROJECT_ID
-gcloud firestore export gs://"$GCLOUD_BUCKET_NAME" --collection-ids=local-product-skins,local-skins,production-product-skins,production-products,production-skins,staging-product-skins,staging-products,staging-skins,test-skins
+node get-collection-list.js
+gcloud firestore export --async gs://"$GCLOUD_BUCKET_NAME" --collection-ids=$(cat collection-list.txt)
