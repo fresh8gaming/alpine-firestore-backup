6db6963cdd20f5aeeae5980fd6e91de43cf89d24
Matt Davies <mattdavies618@gmail.com>
2020-01-27 11:49:32 +0000

    renamed path names in firestore export shells script

--- firestore-export.sh
+++ firestore-export.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 gcloud config set project $GCLOUD_PROJECT_ID
-node get-collection-list.js
-gcloud firestore export --async gs://"$GCLOUD_BUCKET_NAME" --collection-ids=$(cat collection-list.txt)
+node ./get-collection-list.js
+gcloud firestore export --async gs://"$GCLOUD_BUCKET_NAME" --collection-ids=$(cat ./collection-list.txt)
