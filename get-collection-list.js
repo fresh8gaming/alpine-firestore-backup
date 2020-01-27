01c17bbf05e84c0ef20e405e6a8a7ad80db41317
Matt Davies <mattdavies618@gmail.com>
2020-01-27 11:49:51 +0000

    Added error handling in fetch requests

--- get-collection-list.js
+++ get-collection-list.js
@@ -6,19 +6,21 @@ const firestore = new Firestore();
 
 async function main() {
   let collectionList = ''
-  const collections = await firestore.listCollections()
-  
-  for (let i = 0; i < collections.length; i++) {
-    const collection = collections[i]
-    collectionList += collection.id;
-    collectionList += i < collections.length - 1 ? ',' : '';
-  }
+  try {
+    const collections = await firestore.listCollections()
+    
+    for (let i = 0; i < collections.length; i++) {
+      collectionList += collections[i].id;
+      collectionList += i < collections.length - 1 ? ',' : '';
+    }
 
-  process.env.COLLECTION_LIST = collectionList;
-  fs.writeFile(path.resolve(__dirname, 'collection-list.txt'), collectionList, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+    fs.writeFile(path.resolve(__dirname, 'collection-list.txt'), collectionList, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 main();
